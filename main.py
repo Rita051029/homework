310ae8a96f757291493ce224f7acce74aa0e4977
2009m <2009@gmail.com>
2023-11-08 13:51:17 +0200

    dz2

--- main.py
+++ main.py
@@ -5,24 +5,35 @@ class Number1:
     def __init__(self, number, number_2):
         self._number1 = number
         self._number2 = number_2
+        self._number11 = 11
         self.__test_number2 = 1
+        self.__test_number3 = 2
 
-    def _privat_numbers(self, number_3):
+    def __privat_numbers(self, number_3):
         self.__number3 = number_3
-        self.__number4 = 2
+        self.__number4 = 4
         self.__number34 = self.__number3 + self.__number4
         print(self.__number34)
 
+    def _not_privat_numbers(self, number5):
+        self._number5 = number5
+        self._number6 = 6
+        print(self._number5 - self._number6)
+
 class Number(Number1):
 
-    def __printer(self):
-        print(self._number1)
-        print(self._number2)
-        print(min(self._number1,self._number2))
-        print(max(self._number1, self._number2))
-        print(f"{min(self._number1,self._number2)} < {max(self._number1,self._number2)}")
+    def __privat_numbers2(self, number7):
+        self.__number7 = number7
+        self.__number8 = 8
+        print(self.__number7 - self.__number8 - self._number11)
+
+    def _not_privat_numbers2(self, number9):
+        self._number9 = number9
+        self._number10 = 10
+        print(self._number9 + self._number10 + self._number11)
+
 
 number = Number(3, 4)
-#number.__printer()
+number._not_privat_numbers2(10)
 number2 = Number1(4, 2)
-number2._privat_numbers(90)
+number2._not_privat_numbers(90)
