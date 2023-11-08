dfdd18a92be2f5724da9090383ddc059ee5265cc
2009m <2009@gmail.com>
2023-11-08 14:04:41 +0200

    dz2

--- main.py
+++ main.py
@@ -9,31 +9,38 @@ class Number1:
         self.__test_number2 = 1
         self.__test_number3 = 2
 
-    def __privat_numbers(self, number_3):
-        self.__number3 = number_3
-        self.__number4 = 4
-        self.__number34 = self.__number3 + self.__number4
-        print(self.__number34)
+    #def __privat_numbers(self, number_3):
+        #self.__number3 = number_3
+        #self.__number4 = 4
+        #self.__number34 = self.__number3 + self.__number4
+        #print(self.__number34)
 
-    def _not_privat_numbers(self, number5):
-        self._number5 = number5
-        self._number6 = 6
-        print(self._number5 - self._number6)
+    #def _not_privat_numbers(self, number5):
+        #self._number5 = number5
+        #self._number6 = 6
+        #print(self._number5 - self._number6)
 
 class Number(Number1):
 
-    def __privat_numbers2(self, number7):
-        self.__number7 = number7
-        self.__number8 = 8
-        print(self.__number7 - self.__number8 - self._number11)
+    #def __privat_numbers2(self, number7):
+        #self.__number7 = number7
+        #self.__number8 = 8
+        #print(self.__number7 - self.__number8 - self._number11)
 
-    def _not_privat_numbers2(self, number9):
-        self._number9 = number9
-        self._number10 = 10
-        print(self._number9 + self._number10 + self._number11)
+    #def _not_privat_numbers2(self, number9):
+        #self._number9 = number9
+        #self._number10 = 10
+        #print(self._number9 + self._number10 + self._number11)
+
+    def _printer(self, number12, number13):
+        self.__number12 = number12
+        self.__number13 = number13
+        print(min(self.__number12,self.__number13))
+        print(max(self.__number12, self.__number13))
+        print(f"{min(self.__number12,self.__number13)} < {max(self.__number12,self.__number13)}")
 
 
 number = Number(3, 4)
-number._not_privat_numbers2(10)
-number2 = Number1(4, 2)
-number2._not_privat_numbers(90)
+number._printer(12, 13)
+#number2 = Number1(4, 2)
+#number2._not_privat_numbers(90)
